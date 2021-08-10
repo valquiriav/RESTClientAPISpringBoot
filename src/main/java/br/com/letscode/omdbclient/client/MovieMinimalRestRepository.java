cc0279a7676cb04187d973e96503b4bf47eeb980
valquiriav <valvitoli@gmail.com>
2021-08-10 19:15:03 -0300

    Directories

---
+++ src/main/java/br/com/letscode/omdbclient/client/MovieMinimalRestRepository.java
@@ -0,0 +1,11 @@
+package br.com.letscode.omdbclient.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "movieMinimalRest", url = "${omdbapi.url}")
+public interface MovieMinimalRestRepository {
+    @GetMapping
+    br.com.letscode.java.omdbclient.client.ResultSearch search(@RequestParam("s") String movieTitle);
+}
