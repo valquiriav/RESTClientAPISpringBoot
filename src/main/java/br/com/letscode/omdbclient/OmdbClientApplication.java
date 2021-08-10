1153f44e25458fd597c4e73c5b33c3e63174adf7
valquiriav <valvitoli@gmail.com>
2021-08-10 18:29:34 -0300

    Initial commit

---
+++ src/main/java/br/com/letscode/omdbclient/OmdbClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.omdbclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmdbClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/omdbclient/OmdbClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.omdbclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
